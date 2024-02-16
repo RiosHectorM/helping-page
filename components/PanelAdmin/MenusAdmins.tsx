6df8e9fc4e18301437e8074df05de8e224dd9e08
RiosHectorM <rios.hector.martin@gmail.com>
2024-02-16 00:04:44 -0300

    'Panel admin mensajes'

--- components/PanelAdmin/MenusAdmins.tsx
+++ components/PanelAdmin/MenusAdmins.tsx
@@ -3,8 +3,22 @@ import React from 'react';
 const MenusAdmins = () => {
   return (
     <div className="flex flex-wrap justify-around">
+      {/* SORTEO */}
       <div className="card w-96 bg-primary text-primary-content m-8 shadow-secondary shadow-2xl">
-        <div className="card-body">
+        <div className="card-body font-bold">
+          <h2 className="card-title">SORTEO</h2>
+          <p>REALIZAR SORTEO DE TICKETS VENDIDOS</p>
+          <div className="card-actions justify-center">
+            <button className="btn btn-wide">SORTEAR</button>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-wide">Resetear Sorteo</button>
+          </div>
+        </div>
+      </div>
+      {/* compras */}
+      <div className="card w-96 bg-primary text-primary-content m-8 shadow-secondary shadow-2xl">
+        <div className="card-body font-bold">
           <h2 className="card-title">COMPRAS</h2>
           <p>Menu de Administracion de compras de Tickets</p>
           <div className="card-actions justify-center">
@@ -13,14 +27,11 @@ const MenusAdmins = () => {
           <div className="card-actions justify-center">
             <button className="btn btn-wide">Editar Compras</button>
           </div>
-          <div className="card-actions justify-center">
-            <button className="btn btn-wide">Resetear Sorteo</button>
-          </div>
         </div>
       </div>
       {/* PREMIOS */}
       <div className="card w-96 bg-primary text-primary-content m-8 shadow-secondary shadow-2xl">
-        <div className="card-body">
+        <div className="card-body font-bold">
           <h2 className="card-title">PREMIOS</h2>
           <p>Menu de Administracion de Premios</p>
           <div className="card-actions justify-center">
@@ -33,7 +44,7 @@ const MenusAdmins = () => {
       </div>
       {/* Modulo Configuracion */}
       <div className="card w-96 bg-primary text-primary-content m-8 shadow-secondary shadow-2xl">
-        <div className="card-body">
+        <div className="card-body font-bold">
           <h2 className="card-title">CONFIGURACION</h2>
           <p>Menu de configuracion de web</p>
           <div className="card-actions justify-center">
@@ -42,8 +53,18 @@ const MenusAdmins = () => {
           <div className="card-actions justify-center">
             <button className="btn btn-wide">Mensaje de Bienvenida</button>
           </div>
+        </div>
+      </div>
+      {/* Modulo Mensajes */}
+      <div className="card w-96 bg-primary text-primary-content m-8 shadow-secondary shadow-2xl">
+        <div className="card-body font-bold">
+          <h2 className="card-title">INFORMACION / MENSAJES</h2>
+          <p>Menu de configuracion de Mensajes</p>
           <div className="card-actions justify-center">
-            <button className="btn btn-wide">Informacion</button>
+            <button className="btn btn-wide">Cargar Informacion</button>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-wide">Ver Mensajes</button>
           </div>
         </div>
       </div>
